95ab5b8b9ec7e1adc3f7b660ce23a878b812b441
Leonardo <45826897+leonardoernestor@users.noreply.github.com>
2025-04-03 18:23:39 -0300

    Add files via upload

---
+++ perguntas.py
@@ -0,0 +1,7 @@
+nome = "Leonardo"
+sobrenome = "Silva"
+
+
+print("Nome: {nome}")
+print(nome, sobrenome, end="...\n")
+print(nome, sobrenome, sep="#")
--- variaveis_constantes.py
+++ variaveis_constantes.py
@@ -10,5 +10,4 @@ limite_saque_diario = 1000
 BRAZILIAN_STATES =["SP", "RJ", "SC", "RS"]
 
 
-
 print(BRAZILIAN_STATES)
