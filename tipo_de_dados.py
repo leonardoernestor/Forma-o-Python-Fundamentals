4780b8afefac31d6a39bce2d738156bdd42d4c3e
Leonardo <45826897+leonardoernestor@users.noreply.github.com>
2025-04-03 11:44:40 -0300

    Add files via upload

---
+++ tipo_de_dados.py
@@ -0,0 +1,12 @@
+print(11 * 10 + 1000) #
+print(1.5 + 1 + 0.5)
+print(True)
+print(False)
+print("Python")
+print("Python" * 10)
+# print("Python" * 10) - comentarios 
+int()
+float()
+str()
+bool()
+
---
+++ variaveis_constantes.py
@@ -0,0 +1,14 @@
+nome = 'Guilherme'
+idade = 39
+
+nome = 'Giovanna'
+
+print(nome, idade)
+
+limite_saque_diario = 1000
+
+BRAZILIAN_STATES =["SP", "RJ", "SC", "RS"]
+
+
+
+print(BRAZILIAN_STATES)
